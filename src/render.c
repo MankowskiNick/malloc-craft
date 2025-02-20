62cea5a911eaa002fbd23e2e531a798829d76ff7
Nick <nnmankowski@owu.edu>
2025-02-20 14:08:07 -0500

    only render sides next to an empty block

--- src/render.c
+++ src/render.c
@@ -8,6 +8,10 @@
 
 #include <settings.h>
 
+#define VBO_WIDTH 7
+#define VERTS_PER_SIDE 6
+#define SIDE_OFFSET VERTS_PER_SIDE * VBO_WIDTH
+
 VAO vao;
 VBO vbo;
 shader_program program;
@@ -44,66 +48,135 @@ void r_cleanup() {
     delete_program(program);
 }
 
-void render_block(int x_0, int y_0, int z_0, block_type* type, float* block_data) {
-    // copy side info to vertices array
-    for (int i = 0; i < 36; i ++) {
-        
-        // unpack vertex
-        float x = x_0 + CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 0];
-        float y = y_0 + CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 1];
-        float z = z_0 + CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 2];
-        float tx = CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 3];
-        float ty = CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 4];
-        int side = (int)CUBE_VERTICES[i * CUBE_VERTICES_WIDTH + 5];
-        
-        // copy vertex info into vertices array
-        int offset = i * 7;
-        block_data[offset + 0] = x;
-        block_data[offset + 1] = y;
-        block_data[offset + 2] = z;
-        block_data[offset + 3] = tx;
-        block_data[offset + 4] = ty;
-        block_data[offset + 5] = type->face_atlas_coords[side][0];
-        block_data[offset + 6] = type->face_atlas_coords[side][1];
+void render_side(int x_0, int y_0, int z_0, uint side, block_type* type, float* side_data) {
+    int cube_vertices_offset = side * VERTS_PER_SIDE * CUBE_VERTICES_WIDTH;
+    for (int i = 0; i < 6; i++) {
+        int index = cube_vertices_offset + i * CUBE_VERTICES_WIDTH;
+        float x = x_0 + CUBE_VERTICES[index + 0];
+        float y = y_0 + CUBE_VERTICES[index + 1];
+        float z = z_0 + CUBE_VERTICES[index + 2];
+
+        float tx = CUBE_VERTICES[index + 3];
+        float ty = CUBE_VERTICES[index + 4];
+
+        int side_data_offset = i * VBO_WIDTH;
+        side_data[side_data_offset + 0] = x;
+        side_data[side_data_offset + 1] = y;
+        side_data[side_data_offset + 2] = z;
+        side_data[side_data_offset + 3] = tx;
+        side_data[side_data_offset + 4] = ty;
+        side_data[side_data_offset + 5] = type->face_atlas_coords[side][0];
+        side_data[side_data_offset + 6] = type->face_atlas_coords[side][1];
+    }
+}
+
+int get_side_visible(uint side, block_type* blocks[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE], int x, int y, int z) {
+    uint visible = 0;
+    switch(side) {
+        case (int)TOP:
+            if (y + 1 >= CHUNK_HEIGHT) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x][y + 1][z] == NULL;
+            }
+            break;
+        case (int)BOTTOM:
+            if (y - 1 < 0) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x][y - 1][z] == NULL;
+            }
+            break;
+        case (int)BACK:
+            if (x - 1 < 0) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x - 1][y][z] == NULL;
+            }
+            break;
+        case (int)FRONT:
+            if (x + 1 >= CHUNK_SIZE) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x + 1][y][z] == NULL;
+            }
+            break;
+        case (int)LEFT:
+            if (z - 1 < 0) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x][y][z - 1] == NULL;
+            }
+            break;
+        case (int)RIGHT:
+            if (z + 1 >= CHUNK_SIZE) {
+                visible = 1;
+            }
+            else {
+                visible = blocks[x][y][z + 1] == NULL;
+            }
+            break;
+        default:
+            visible = 1;
+    }
+    return visible;
+}
+
+void render_block(
+    float** side_data, 
+    int* num_sides, 
+    int x, int y, int z,
+    block_type* blocks[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE]
+) {
+    for (int side = 0; side < 6; side++) {
+        if (!get_side_visible(side, blocks, x, y, z)) {
+            continue;
+        }
+
+        int new_side_count = (*num_sides) + 1;
+        float* tmp = realloc(*side_data, new_side_count * SIDE_OFFSET * sizeof(float));
+        assert(tmp != NULL && "Failed to allocate memory for side data");
+        *side_data = tmp;
+        block_type* type = blocks[x][y][z];
+        render_side(
+            x, y, z, 
+            side, 
+            type,
+            (*side_data) + (*num_sides) * SIDE_OFFSET
+        );
+        (*num_sides)++;
     }
 }
 
 void render_chunk(chunk* c) {
-    uint block_offset = 6 * 6 * 7;
-
-    float* block_data = malloc(block_offset * sizeof(float));
-    int num_blocks = 0;
+    float* side_data = malloc(SIDE_OFFSET * sizeof(float));
+    int num_sides = 0;
     for (int i = 0; i < CHUNK_SIZE; i++) {
         for (int j = 0; j < CHUNK_SIZE; j++) {
             for (int k = 0; k < CHUNK_HEIGHT; k++) {
                 if (c->blocks[i][k][j] == NULL) {
                     continue;
                 }
-
-                float* tmp = realloc(block_data, (num_blocks + 1) * block_offset * sizeof(float));
-                assert(tmp != NULL && "Failed to allocate memory for block data");
-                block_data = tmp;
-                int offset = num_blocks * block_offset;
-                render_block(
-                    i, k, j, 
-                    c->blocks[i][k][j], 
-                    block_data + offset
-                );
-                num_blocks++;
+                render_block(&side_data, &num_sides, i, k, j, c->blocks);
             }
         }
     }
 
     bind_vao(vao);
-    buffer_data(vbo, GL_STATIC_DRAW, block_data, num_blocks * block_offset * sizeof(float));
-    add_attrib(&vbo, 0, 3, 0, 7 * sizeof(float));
-    add_attrib(&vbo, 1, 2, 3 * sizeof(float), 7 * sizeof(float));
-    add_attrib(&vbo, 2, 2, 5 * sizeof(float), 7 * sizeof(float));
+    buffer_data(vbo, GL_STATIC_DRAW, side_data, num_sides * SIDE_OFFSET * sizeof(float));
+    add_attrib(&vbo, 0, 3, 0, VBO_WIDTH * sizeof(float));
+    add_attrib(&vbo, 1, 2, 3 * sizeof(float), VBO_WIDTH * sizeof(float));
+    add_attrib(&vbo, 2, 2, 5 * sizeof(float), VBO_WIDTH * sizeof(float));
     use_vbo(vbo);
 
-    glDrawArrays(GL_TRIANGLES, 0, num_blocks * 36);
+    glDrawArrays(GL_TRIANGLES, 0, num_sides * VERTS_PER_SIDE);
 
-    free(block_data);
+    free(side_data);
 }
 
 void render(camera cam, shader_program program) {
